d2dcbf7e408228c316fe5334811495ea360e06e1
Rik <5927342+rikwillems@users.noreply.github.com>
2019-12-18 08:08:34 +0100

    Add files via upload

---
+++ Console/Command/UnholdCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Sample\Order\Console\Command;
+
+use Magento\Framework\App\Area;
+use Magento\Framework\App\State;
+use Magento\Sales\Model\Service\OrderService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class UnholdCommand extends Command
+{
+    /** @var State */
+    protected $state;
+
+    /** @var OrderService */
+    private $orderService;
+
+    public function __construct(
+        State $state,
+        OrderService $orderService
+    ) {
+        parent::__construct();
+        $this->state = $state;
+        $this->orderService = $orderService;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $this->setName('sample:order:unhold');
+        $this->setDescription('Unhold an order');
+        $this->addArgument('order_id', InputArgument::REQUIRED, 'Order ID to unhold.');
+        parent::configure();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->state->setAreaCode(Area::AREA_ADMINHTML);
+        $orderId = $input->getArgument('order_id');
+        $this->orderService->unhold($orderId);
+    }
+}
